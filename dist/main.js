05f8093885d673ad5d1e826ca9c8c1b6063c1c4b
qqdtf98 <qqdtf98@gmail.com>
2020-05-26 17:52:37 +0900

    feat: install cors module

--- dist/main.js
+++ dist/main.js
@@ -4,13 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
 var express_1 = __importDefault(require("express"));
+var getTodoDoneList_1 = require("./server/getTodoDoneList");
 var getUserData_1 = __importDefault(require("./server/getUserData"));
 var app = express_1.default();
+app.use(cors_1.default());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
 app.get('/user/get', function (req, res) {
     getUserData_1.default(res, req.query);
+});
+app.post('/list/get', function (req, res) {
+    getTodoDoneList_1.getTodoDoneList(res, req.body.data);
 });
 app.listen(80, function () {
     console.log('Server is running...');
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { Query } from './interfaces/todo-list-type'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import express from 'express'
+import { getTodoDoneList } from './server/getTodoDoneList'
 import getUserData from './server/getUserData'
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/user/get', function (req: express.Request, res: express.Response) {
   getUserData(res, (req.query as unknown) as Query)
+})
+app.post('/list/get', function (req: express.Request, res: express.Response) {
+  getTodoDoneList(res, req.body.data)
 })
 
 app.listen(80, function () {
